16e3950d5aebef4ebf0a0fc55085fc05227c11ee
Kirill <ivanteev87@mail.ru>
2022-05-10 02:09:35 +0300

    new program work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 31. Задать массив из 8 элементов и вывести их на экран
+
+int N = 5;
+
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i]} ");
+}
+
+PrintArray(SetArray(N));
